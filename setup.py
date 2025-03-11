f0282bc508f1e0613500f095b132a4589ecfc270
Ghassen Saidi <124514548+ghassenTn@users.noreply.github.com>
2025-03-11 11:08:58 +0100

    new release 1.2.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="ChartForgeTK",
-    version="1.1.0",
+    version="1.2.0",
     packages=["ChartForgeTK"],  
     package_dir={"ChartForgeTK": "ChartForgeTK"}, 
     install_requires=[
